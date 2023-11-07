c559fa173d501d094b5ffde67ce026f3b6d0bd0e
[satuhalinen] <satuh9@gmail.com>
2023-11-07 22:41:03 +0200

    remove animals added

--- zoo/src/App.jsx
+++ zoo/src/App.jsx
@@ -2,15 +2,35 @@ import Card from './Card';
 import Header from './Header';
 import Footer from './Footer';
 import './App.css';
+import {useState} from 'react';
+import {animals, birds} from './animalsList' 
 
 function App() {
+
+  const [organisms, setOrganisms] = useState(animals.concat(birds)
+    
+  );
+
+  const removeHandler = (name) => {
+    const updatedArray = organisms.filter(animal => animal.name !== name)
+    setOrganisms(updatedArray)
+  }
+  
   return (
     <>
     <Header/>
     <main>
       <h1>Animals</h1>
       <div className="cards">
-        <Card/>
+        {organisms.map((animal) => (
+ <Card
+ key={animal.name}
+ {...animal}
+ click = {() => removeHandler(animal.name)}
+  
+       
+       />
+         ))}
       </div>
     </main>
     <Footer/>
