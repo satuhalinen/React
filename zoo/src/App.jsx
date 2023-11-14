2e3bfb1f0b0f4323e0a576e98129d6ede13e9999
[satuhalinen] <satuh9@gmail.com>
2023-11-14 21:38:43 +0200

    fix close button

--- zoo/src/App.jsx
+++ zoo/src/App.jsx
@@ -1,77 +1,64 @@
-import Card from './Card';
-import Header from './Header';
-import Footer from './Footer';
-import './App.css';
-import {useState} from 'react';
-import {animals, birds} from './animalsList' 
+import Card from "./Card";
+import Header from "./Header";
+import Footer from "./Footer";
+import "./App.css";
+import { useState } from "react";
+import { animals, birds } from "./animalsList";
 
 function App() {
+  const [organisms, setOrganisms] = useState(animals.concat(birds));
 
-  const [organisms, setOrganisms] = useState(animals.concat(birds)
-    
-  );
-
-  const [search, setSearch] = useState('');
-
+  const [search, setSearch] = useState("");
 
   const removeHandler = (name) => {
-    const updatedArray = organisms.filter(animal => animal.name !== name)
-    setOrganisms(updatedArray)
-  }
-  
-  const searchHandler = (e) =>{
+    const updatedArray = organisms.filter((animal) => animal.name !== name);
+    setOrganisms(updatedArray);
+  };
+
+  const searchHandler = (e) => {
     setSearch(e.target.value);
-  }
+  };
   const likesHandler = (name, action) => {
     const updatedArray = organisms.map((animal) => {
-      if (animal.name === name){
-        if (action === 'add'){
-          return {...animal, likes: animal.likes + 1}
+      if (animal.name === name) {
+        if (action === "add") {
+          return { ...animal, likes: animal.likes + 1 };
         }
-        if (action === 'away') {
-          return {...animal, likes: animal.likes - 1}
+        if (action === "away") {
+          return { ...animal, likes: animal.likes - 1 };
         }
       } else {
         return animal;
       }
+    });
 
-    })
-
-
-
-
-
-
-setOrganisms(updatedArray);
-
-  }
+    setOrganisms(updatedArray);
+  };
 
   return (
     <>
-    <Header/>
-    <main>
-      <h1>Animals</h1>
-      <input type="text" onChange={searchHandler} />
-      <div className="cards">
-        {organisms.filter(organism => organism.name.toLowerCase().includes(search.toLowerCase())).map((animal) => (
- <Card
- key={animal.name}
- {...animal}
- onRemove = {() => removeHandler(animal.name, 'remove')}
-
- addLikes={() => likesHandler(animal.name, 'add')}
-removeLikes={() => likesHandler(animal.name, 'away')}
-       
-       />
-         ))}
-      </div>
-      
-    </main>
-    <Footer/>
+      <Header />
+      <main>
+        <h1>Animals</h1>
+        <input type="text" onChange={searchHandler} />
+        <div className="cards">
+          {organisms
+            .filter((organism) =>
+              organism.name.toLowerCase().includes(search.toLowerCase())
+            )
+            .map((animal) => (
+              <Card
+                key={animal.name}
+                {...animal}
+                onRemove={() => removeHandler(animal.name)}
+                addLikes={() => likesHandler(animal.name, "add")}
+                removeLikes={() => likesHandler(animal.name, "away")}
+              />
+            ))}
+        </div>
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
-export default App
-
-
-
+export default App;
--- zoo/src/Card.jsx
+++ zoo/src/Card.jsx
@@ -1,30 +1,30 @@
-const Card = ({name, click, likes, addLikes, removeLikes}) => { 
-  
+const Card = ({ name, likes, addLikes, removeLikes, onRemove }) => {
   return (
-  <div className="card">
-     <button className="close" onClick={click}>close</button>
-    <img src={`https://source.unsplash.com/400x400/?${name}`} alt={name} />
+    <div className="card">
+      <button className="close" onClick={onRemove}>
+        close
+      </button>
+      <img src={`https://source.unsplash.com/400x400/?${name}`} alt={name} />
 
-  
-    <h2>{name}</h2> 
-    <div className="counts">
-    <button onClick={addLikes}><span className="material-symbols-outlined thumb">thumb_up</span></button>
-   
-<div className="likes_container">
-<p id="amount">{likes}</p>
-<span id="favorite" className="material-symbols-outlined">{likes >= 0 ? 'favorite' : 'heart_broken'}</span>
-    </div>
+      <h2>{name}</h2>
+      <div className="counts">
+        <button onClick={addLikes}>
+          <span className="material-symbols-outlined thumb">thumb_up</span>
+        </button>
 
-    <button onClick={removeLikes}><span className="material-symbols-outlined">thumb_down</span></button>
+        <div className="likes_container">
+          <p id="amount">{likes}</p>
+          <span id="favorite" className="material-symbols-outlined">
+            {likes >= 0 ? "favorite" : "heart_broken"}
+          </span>
+        </div>
+
+        <button onClick={removeLikes}>
+          <span className="material-symbols-outlined">thumb_down</span>
+        </button>
+      </div>
     </div>
-    
-  </div>
   );
 };
 
 export default Card;
-
-
-
-
-
