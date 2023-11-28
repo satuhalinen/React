28b011384facc5f88e1918b3c24f833fa85c114b
[satuhalinen] <satuh9@gmail.com>
2023-11-28 14:42:39 +0200

    add conditional screens for launch, game on and game over

--- speed_game_react/src/App.jsx
+++ speed_game_react/src/App.jsx
@@ -3,11 +3,15 @@ import NewGame from "./components/NewGame";
 import { levels } from "./levels";
 import Circle from "./UI_components/Circle";
 import Game from "./components/Game";
+import GameOver from "./components/GameOver";
 
 function App() {
   const [player, setPlayer] = useState();
   const [circles, setCircles] = useState([]);
   const [score, setScore] = useState(0);
+  const [gameLaunch, setGameLaunch] = useState(true);
+  const [gameOn, setGameOn] = useState(false);
+  const [gameOver, setGameOver] = useState(false);
 
   const gameSetHandler = (level, name) => {
     const levelIndex = levels.findIndex((el) => el.name === level);
@@ -27,14 +31,25 @@ function App() {
       level: level,
       name: name,
     });
+
+    setGameLaunch(!gameLaunch);
+    setGameOn(!gameOn);
+  };
+
+  const stopHandler = () => {
+    setGameOn(!gameOn);
+    setGameOver(!gameOver);
   };
   console.log(player);
 
   return (
     <>
       <h1>Freeze the berries!</h1>
-      <NewGame onclick={gameSetHandler} />
-      <Game score={score} circles={circles} />
+      {gameLaunch && <NewGame onclick={gameSetHandler} />}
+      {gameOn && (
+        <Game score={score} circles={circles} stopHandler={stopHandler} />
+      )}
+      {gameOver && <GameOver />}
     </>
   );
 }
---
+++ speed_game_react/src/components/GameOver.jsx
@@ -0,0 +1,5 @@
+function GameOver() {
+  return <div>game over will be here</div>;
+}
+
+export default GameOver;
