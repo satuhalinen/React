a95f8fcea24f7c992763f6fcbc6099cc43385483
[satuhalinen] <satuh9@gmail.com>
2023-11-30 17:19:57 +0200

    update GameOver with overlay and close button

--- speed_game_react/src/components/GameOver.jsx
+++ speed_game_react/src/components/GameOver.jsx
@@ -1,5 +1,17 @@
-function GameOver() {
-  return <div>game over will be here</div>;
+function GameOver({ name, score, level, closeHandler }) {
+  return (
+    <div className="overlay">
+      <div className="modal">
+        <h2>Game over</h2>
+        <div>
+          {name}, you collected {score} berries on level {level}.
+        </div>
+        <button className="close" onClick={closeHandler}>
+          <span className="material-symbols-outlined"> Close </span>
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default GameOver;
