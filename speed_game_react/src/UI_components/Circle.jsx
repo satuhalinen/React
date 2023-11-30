1b46a3ec1735b1107be5a3bdb87557d6b3bb9eae
[satuhalinen] <satuh9@gmail.com>
2023-11-30 14:02:26 +0200

    add berry picture backgrounds for cubes

--- speed_game_react/src/UI_components/Circle.jsx
+++ speed_game_react/src/UI_components/Circle.jsx
@@ -1,6 +1,28 @@
 function Circle({ id }) {
+  let squareName;
+  if (id == 0) {
+    squareName = "square0";
+  }
+  if (id == 1) {
+    squareName = "square1";
+  }
+  if (id == 2) {
+    squareName = "square2";
+  }
+  if (id == 3) {
+    squareName = "square3";
+  }
+  if (id == 4) {
+    squareName = "square4";
+  }
+  if (id == 5) {
+    squareName = "square5";
+  }
+  if (id == 6) {
+    squareName = "square6";
+  }
   return (
-    <div className="circle">
+    <div className="circle" id={squareName}>
       <p>{id}</p>
     </div>
   );
