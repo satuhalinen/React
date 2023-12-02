e0edaaeb28ff65d3fbe4254d996efee7625c459e
[satuhalinen] <satuh9@gmail.com>
2023-12-02 14:24:00 +0200

    add conditional rendering for gameover text

--- speed_game_react/src/components/GameOver.jsx
+++ speed_game_react/src/components/GameOver.jsx
@@ -1,10 +1,17 @@
-function GameOver({ name, score, level, closeHandler }) {
+function GameOver({ name, score, level, closeHandler, text }) {
+  if (score < 500) {
+    text = ` The amount of berries you froze is not enough even for one winter month.`;
+  } else if (score >= 500 && score < 1000) {
+    text = ` The amount is enough for almost the whole winter.`;
+  } else {
+    text = ` Great, you have enough berries for the whole winter.`;
+  }
   return (
     <div className="overlay">
       <div className="modal">
         <h2>Game over</h2>
         <div>
-          {name}, you collected {score} berries on level {level}.
+          {name}, you froze {score} berries on level {level}.{text}
         </div>
         <button className="close" onClick={closeHandler}>
           <span className="material-symbols-outlined"> Close </span>
