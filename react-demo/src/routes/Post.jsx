7105a976e0f6f283666f1033b219383b606dabb9
[satuhalinen] <satuh9@gmail.com>
2023-12-13 16:10:55 +0200

    change tasks to be visible only when not completed

--- react-demo/src/routes/Post.jsx
+++ react-demo/src/routes/Post.jsx
@@ -1,11 +1,13 @@
 function Post({ id, title, completed }) {
-  return (
-    <>
-      <p>{id}</p>
-      <h2>{title}</h2>
-      <p>{completed}</p>
-    </>
-  );
+  if (completed !== true) {
+    return (
+      <>
+        <p>{id}</p>
+        <h2>{title}</h2>
+        <p>{completed}</p>
+      </>
+    );
+  }
 }
 
 export default Post;
