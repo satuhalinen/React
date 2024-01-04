d60a82a89c6c6432accc7236a44f2fc7a899eb4f
[satuhalinen] <satuh9@gmail.com>
2024-01-04 15:02:12 +0200

    add the final exercise 5

--- basics/exercise5/src/App.jsx
+++ basics/exercise5/src/App.jsx
@@ -1,5 +1,3 @@
-// Your goal is to change the title, author, genre and reading status when the button in the App component is pressed
-// You DON'T need to fetch the values entered into the <input /> fields
 import "./styles.css";
 
 export const book = {
@@ -9,13 +7,16 @@ export const book = {
   isRead: true,
 };
 
-// Please note: The Add book does not actually work!
-// This exercise is just about practicing event handling
-// Ignore the inputs; you'll learn to handle user input later.
-
 function App() {
-  /* Implement an event handler function, handleClick, 
-  that is triggered when the "Add book" button is clicked. */
+  function handleClick() {
+    console.log(book);
+    book.title = "Eloquent Javascript";
+    book.author = "Marijn Haverbeke";
+    book.genre = "Science Fiction";
+    book.isRead = true;
+    return console.log(book);
+  }
+
   return (
     <div id="app">
       <h1>Add book</h1>
@@ -38,7 +39,7 @@ function App() {
         </select>
       </div>
 
-      <button>Add book</button>
+      <button onClick={handleClick}>Add book</button>
     </div>
   );
 }
