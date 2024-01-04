915a3af291db131625a123b293758a8c878e66ba
[satuhalinen] <satuh9@gmail.com>
2024-01-04 11:25:22 +0200

    add basefiles

---
+++ basics/exercise3/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+const App = () => {
+  return (
+    <div className="books">
+      {/* render at least two instances of the BookInfo component.
+       */}
+    </div>
+  );
+};
+
+export default App;
