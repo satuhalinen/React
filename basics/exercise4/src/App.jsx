a6184adcce06bfecebba80e83043de107605a3a0
[satuhalinen] <satuh9@gmail.com>
2024-01-04 13:48:06 +0200

    add base files

---
+++ basics/exercise4/src/App.jsx
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+const App = () => {
+  return (
+    <div className="books">
+      {/*  To test the solution, import the BookCard component and uncomment the code. */}
+      {/*  <BookCard title="The Art of Programming">
+        An insightful journey through the world of programming, exploring both
+        theory and practical applications.
+        <button>Read More</button>
+      </BookCard> */}
+    </div>
+  );
+};
+
+export default App;
