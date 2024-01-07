94c14ab759eabec3f19cbed06d5c17637b7d8cf3
[satuhalinen] <satuh9@gmail.com>
2024-01-07 09:37:30 +0200

    make data of books visible

--- basics/exercise10/src/App.jsx
+++ basics/exercise10/src/App.jsx
@@ -1,9 +1,11 @@
 import "./styles.css";
+import BookList from "./BookList";
 
 const App = () => {
   return (
     <main>
       <h1>List and keys</h1>
+      <BookList></BookList>
       {/* TODO: import BookList component to display the list of the books
        */}
     </main>
--- basics/exercise10/src/BookList.jsx
+++ basics/exercise10/src/BookList.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Book from "./Book";
+import { books } from "./books";
 
 const BookList = () => {
   const [activeBookId, setActiveBookId] = useState(null);
@@ -10,6 +11,17 @@ const BookList = () => {
 
   return (
     <div className="card-grid">
+      {books.map(function (data) {
+        return (
+          <div>
+            <Book
+              title={data.title}
+              author={data.author}
+              description={data.description}
+            ></Book>
+          </div>
+        );
+      })}
       {/* TODO: Implement mapping of the books array from books.js to render each book using the Book component. 
       Ensure each Book component receives the necessary props, including a showDescription prop to determine 
       if the current book's description should be displayed based on its ID comparison with activeBookId. */}
